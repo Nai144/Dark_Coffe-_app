764acfb5bb508837ae9323431e2613cdaca3209a
Ian Javier <ipjavier144@gmail.com>
2024-09-27 17:36:30 -0300

    Creacion delos buttonNavigation y las cArd

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,27 @@ void main() {
     home: HomePage(),
   ));
 }
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+ int _selectedIndex = 1;
+
+ static const List<Widget> _pages = <Widget>[
+    MyFavoriteScreen(),
+    WelcomeScreen(),
+    MyPreferencesScreen(),
+  ];
+  
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,64 +35,133 @@ class HomePage extends StatelessWidget {
         title: const Text('Home Page'),
       ),
       body: Center(
+        child: _pages.elementAt(_selectedIndex),       
+      ),
+      bottomNavigationBar: createBottomNavigationBar(),     
+    );
+  }
+
+  BottomNavigationBar createBottomNavigationBar() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.hotel_class_sharp),
+          label: 'Favorites',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.house_siding),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.liquor_outlined),
+          label: 'Preferences',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: _onItemTapped,
+    );
+  }
+}
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: const Text('Search All Screen'),
-              onPressed: () {
-                Navigator.push( context,
-                MaterialPageRoute(builder: (context) => const SearchAllScreen()),
-                );
-              },
+          children: <Widget>[
+            Card(
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 270.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: const DecorationImage(
+                        image : NetworkImage('https://files.lafm.com.co/assets/public/styles/seoimg_1200x675_/public/2022-04/cafe_0_9.jpg?VersionId=kGk1XrKgHp2FQTfPHhVPm9pqwpkgMfE1&h=b1f6ba5c&itok=2Z3xtfVB'),
+                        fit: BoxFit.cover
+                      )
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Card 1', style: TextStyle(fontSize: 18)),
+                  ),
+                ],
+              ),
             ),
-            ElevatedButton(
-              child: const Text('Search Review Screen'),
-              onPressed: () {
-                Navigator.push( context,
-                MaterialPageRoute(builder: (context) => const SearchReviewScreen()),
-                );
-              },
-            ),
-            ElevatedButton(
-              child: const Text('Search Recipes Screen'),
-              onPressed: () {
-                Navigator.push( context,
-                MaterialPageRoute(builder: (context) => const SearchRecipesScreen()),
-                );
-              },
+            const SizedBox(height: 16.0),
+            Card(
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 270.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: const DecorationImage(
+                        image : NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmS0RqB5qc2fk9z1JyjzqiKExTiKZrX-nIaA&s'),
+                        fit: BoxFit.cover
+                      )
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Card 2', style: TextStyle(fontSize: 18)),
+                  ),
+                ],
+              ),
             ),
           ],
-        ), 
-      ),
-      persistentFooterButtons: <Widget>[
-        IconButton(icon: const Icon(Icons.liquor_outlined), //Go to MY Prefernces
-          onPressed: () {
-                Navigator.push( context,
-                MaterialPageRoute(builder: (context) => const MyPreferencesScreen()),
-                );
-          },
-          padding: const EdgeInsets.all(45.0),
-        ),       
-        IconButton(icon: const Icon(Icons.hotel_class_sharp), //Go to MY favorites
-          onPressed: () {
-                Navigator.push( context,
-                MaterialPageRoute(builder: (context) => const MyFavoriteScreen()),
-                );
-          },
-          padding: const EdgeInsets.all(45.0),
         ),
-        IconButton(icon: const Icon(Icons.library_add_sharp), //Go to MY recipes
+      ),
+    );
+  }
+}
+class MyFavoriteScreen extends StatelessWidget {
+  const MyFavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mis Favoritos Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
           onPressed: () {
-                Navigator.push( context,
-                MaterialPageRoute(builder: (context) => const MyRecipesScreen()),
-                );
+            Navigator.pop(context);
           },
-          padding: const EdgeInsets.all(45.0),
-          mainAxisAlignment: MainAxisAlignment.start
-        ),   
-      ],
-      
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+class MyPreferencesScreen extends StatelessWidget {
+  const MyPreferencesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mis Preferencias Screen'),
+      ),
+      body: Center(
+        child: 
+        ElevatedButton(
+          child: const Text('My Preferences List Screen'),
+            onPressed: () {
+              Navigator.push( context,
+              MaterialPageRoute(builder: (context) => const MyPreferencesListScreen()),
+              );
+          },
+        ),
+      ),
     );
   }
 }
@@ -139,69 +226,7 @@ class SearchRecipesScreen extends StatelessWidget {
     );
   }
 }
-class MyRecipesScreen extends StatelessWidget {
-  const MyRecipesScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Recipes Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
-  }
-}
-class MyFavoriteScreen extends StatelessWidget {
-  const MyFavoriteScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mis Favoritos Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
-  }
-}
-class MyPreferencesScreen extends StatelessWidget {
-  const MyPreferencesScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mis Preferencias Screen'),
-      ),
-      body: Center(
-        child: 
-        ElevatedButton(
-          child: const Text('My Preferences List Screen'),
-            onPressed: () {
-              Navigator.push( context,
-              MaterialPageRoute(builder: (context) => const MyPreferencesListScreen()),
-              );
-          },
-        ),
-      ),
-    );
-  }
-}
 class MyPreferencesListScreen extends StatelessWidget {
   const MyPreferencesListScreen({super.key});
 
